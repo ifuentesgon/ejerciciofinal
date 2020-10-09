379d6fb550c2f56d850e65cd40262ed302292a57
ifuentesgon <40896830+ifuentesgon@users.noreply.github.com>
2020-10-09 19:53:54 -0300

    Add files via upload

---
+++ jenkins/deploy/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Generamos
+echo app > /tmp/.auth
+echo $BUILD_TAG >> /tmp/.auth
+
+# Transifere el archivo
+scp -i /tmp/tf_pipeline/class_key.pem /tmp/.auth ubuntu@ec2-3-16-69-38.us-east-2.compute.amazonaws.com:/tmp/.auth
+scp -i /tmp/tf_pipeline/class_key.pem /tmp/jenkins/deploy/publish ubuntu@ec2-3-16-69-38.us-east-2.compute.amazonaws.com:/tmp/publish
+ssh -i /tmp/tf_pipeline/class_key.pem ubuntu@ec2-3-16-69-38.us-east-2.compute.amazonaws.com /tmp/publish
